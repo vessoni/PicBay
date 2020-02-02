d88cb7581cc46c0b6f77c1f9e2d9b4f30cbb9fa9
Alexandre Vessoni <av.vessoni@gmail.com>
2020-02-02 20:09:39 -0300

    Change Key Api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = '15100746-99cd627894bf91afe37d0033e';
+const apiKey = '6473511-0417f2cad683f1bee54cafe15';
 const api = axios.create({
   baseURL: `https://pixabay.com/api/?key=${apiKey}`,
 });
